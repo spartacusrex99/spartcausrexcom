8db60879003d1f01dfed394d643f5e81b0c999e8
spartacusrex <patrickcerri@gmail.com>
2023-09-26 09:41:26 +0100

    start home page

---
+++ web/sparx.js
@@ -0,0 +1,24 @@
+
+/**
+ * Write the header
+ */
+function writeHeader(){
+	
+	var gap = "&nbsp;&nbsp;&nbsp;";
+	
+	document.write(""
+		+"<br><center><h1>Spartacus Rex</h1>"
+		+"<button class='solobutton' onclick='location.href=\"index.html\";'>Home</button>"+gap
+		+"<button class='solobutton' onclick='location.href=\"runanode.html\";'>Run a node</button>"+gap
+		+"<button class='solobutton' onclick='location.href=\"aboutme.html\";'>Downloads</button>"+gap
+		+"<button class='solobutton' onclick='location.href=\"blog.html\";'>Blog</button>"+gap
+		+"<br><br>"
+		+"");
+} 
+
+/**
+ * Write the footer
+ */
+function writeFooter(){
+	document.write("</center>");
+} 
